b763d65925dc58a2c9ab75ef2bc474addd1d93c5
pirulitomagico44-create <pirulitomagico44-create@users.noreply.github.com>
2026-09-20 20:38:45 -0300

    testar recebimento de mensagens

--- dados/src/connect.js
+++ dados/src/connect.js
@@ -1517,7 +1517,7 @@ async function createBotSocket(authDir) {
             if (messagesListenerAttached) return;
             messagesListenerAttached = true;
 
-            NazunaSock.ev.on('messages.upsert', async (m) => {
+            NazunaSock.ev.on('messages.upsert', async (m) => { console.log("📩 MENSAGEM RECEBIDA PELO LISTENER");
                 if (!m.messages || !Array.isArray(m.messages)) return;
 
 
